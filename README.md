898f1fbf6d45bc440a9f5c2a2ae7950d09dc4b5e
Minchul Kim <minchul.kim@snu.ac.kr>
2019-07-21 11:31:14 +0900

    fixing code for readiblity

--- README.md
+++ README.md
@@ -20,7 +20,28 @@ $ vagrant ssh
 $ vagrant up
 ```
 
-# RUN
+3. Create DB
+
+```
+psql -d news -f newsdata.sql
+```
+
+3. Create Views in the DB for easier coding and db manipulation.
+
+```
+    CREATE VIEW error_count AS
+    SELECT to_char(log.time, 'Mon DD, YYYY') as day, count(*) as sum FROM log \n
+    where log.status NOT LIKE '200 OK' \n
+    GROUP BY day;
+``` 
+
+```
+    CREATE VIEW all_count AS
+    SELECT to_char(log.time, 'Mon DD, YYYY') as day, count(*) as sum FROM log \n
+    GROUP BY day;
+```
+
+# Run
 
 This python script will read newsdata.sql and print a statement in result.txt file that answers the three questions.
 1. What are the most popular three articles of all time?
@@ -28,7 +49,7 @@ This python script will read newsdata.sql and print a statement in result.txt fi
 3. On which days did more than 1% of requests lead to errors?
 
 Run with the following code. The question 3 needs views with error_count and all_count. 
-They will be created if the code has never been run before. The code will skip if the view already exists.
+
 
 ```
 cd into/the/current_dir
--- project1.py
+++ project1.py
@@ -1,10 +1,39 @@
 #!/usr/bin/env python3
 
 import psycopg2
+import sys
+
+
+def connect(database_name="news"):
+    try:
+        db = psycopg2.connect("dbname={}".format(database_name))
+        cursor = db.cursor()
+        return db, cursor
+    except psycopg2.Error, e:
+        print ("Unable to connect!")
+        print (e.pgerror)
+        print (e.diag.message_detail)
+        sys.exit(1)
+
+def execute_query(db, cursor, query):
+    cursor.execute(query)
+    results = c.fetchall()
+    db.close()
+    return results
+
+def formatted_print(results, type="views"):
+    for result in results:
+        first, second = result
+        if type=="views":
+            print("    {} - {} views".format(first, second))
+        elif type=="error":
+            print("    {} - {} % error".format(first, second))
+        else:
+            raise ValueError
+    print("-" * 70)
 
 
 if __name__ == "__main__":
-
 
     # What are the most popular three articles of all time?
     # Present this information as a sorted list with the most popular article at the top.
@@ -15,23 +44,12 @@ if __name__ == "__main__":
     GROUP BY title ORDER BY n_views DESC LIMIT 3;
     """
 
+    db, c = connect()
+    results = execute_query(db, c, query)
 
-    try:
-        db = psycopg2.connect(database="news")
-    except psycopg2.Error as e:
-        print ("Unable to connect!")
-        print (e.pgerror)
-        print (e.diag.message_detail)
-        sys.exit(1)
-    else:
-        print ("Connected!")
+    print("Q1: What are the most popular three articles of all time?")
+    formatted_print(results)
 
-    c = db.cursor()
-    c.execute(query)
-    results = c.fetchall()
-    db.close()
-    print("Q1")
-    print(results)
 
     # Who are the most popular article authors of all time?
     query = """
@@ -42,59 +60,21 @@ if __name__ == "__main__":
     ORDER BY n_article DESC;
     """
 
-    db = psycopg2.connect(database="news")
-    c = db.cursor()
-    c.execute(query)
-    results = c.fetchall()
-    db.close()
-    print("Q2")
-    print(results)
+    db, c = connect()
+    results = execute_query(db, c, query)
+
+    print("Q2: Who are the most popular article authors of all time?")
+    formatted_print(results)
 
     # On which days did more than 1% of requests lead to errors?
-    view_create = """
-    CREATE VIEW error_count AS
-    SELECT to_char(log.time, 'Mon DD, YYYY') as day, count(*) as sum FROM log \n
-    where log.status NOT LIKE '200 OK' \n
-    GROUP BY day;
-    """
-    try:
-        # if the table doesnt exist
-        db = psycopg2.connect(database="news")
-        c = db.cursor()
-        c.execute(view_create)
-        db.commit()
-        db.close()
-    except:
-        # print("error_count view exits")
-        pass
-
-    view_create = """
-    CREATE VIEW all_count AS
-    SELECT to_char(log.time, 'Mon DD, YYYY') as day, count(*) as sum FROM log \n
-    GROUP BY day;
-    """
-    try:
-        db = psycopg2.connect(database="news")
-        c = db.cursor()
-        c.execute(view_create)
-        db.commit()
-        db.close()
-    except:
-        # print("all_count view exitss")
-        pass
-
-
     query = """
-    SELECT CAST(error_count.sum as NUMERIC) / CAST(all_count.sum as NUMERIC) * 100 as result from all_count \n
+    SELECT all_count.day, CAST(error_count.sum as NUMERIC) / CAST(all_count.sum as NUMERIC) * 100 as result from all_count \n
     JOIN error_count on error_count.day = all_count.day \n
     ORDER BY result desc limit 1;
     """
-    db = psycopg2.connect(database="news")
-    c = db.cursor()
-    c.execute(query)
-    results = c.fetchall()
-    db.close()
-    print("Q3")
-    print(results)
 
+    db, c = connect()
+    results = execute_query(db, c, query)
 
+    print("Q3: On which days did more than 1% of requests lead to errors?")
+    formatted_print(results, type="error")
